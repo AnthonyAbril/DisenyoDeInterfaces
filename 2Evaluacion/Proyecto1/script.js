aa04d39ec8839b6534fd41ae7ee385eac74744e1
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-12-09 20:12:41 +0100

    tamaño xl de PC

--- 2Evaluacion/Proyecto1/script.js
+++ 2Evaluacion/Proyecto1/script.js
@@ -1,26 +1,3 @@
-
-// Para cada item del menu nav
-document.querySelectorAll('nav a').forEach(anchor => {
-    
-    // Se le añade la funcion al pulsarlo
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // Evita el comportamiento predeterminado del enlace
-
-        const targetId = this.getAttribute('href'); // Obtiene el id objetivo
-
-        const targetElement = document.querySelector(targetId); // Guarda el elemento de ese id
-
-        // Si existe ese elemento
-        if (targetElement) {
-            // Baja hasta el elemento con
-            targetElement.scrollIntoView({
-                behavior: 'smooth', // Desplazamiento suave
-                block: 'start'     // Alineación al inicio del elemento
-            });
-        }
-    });
-});
-
 //Boton de subir
 const btnSubir = document.getElementById('btnSubir');
 
