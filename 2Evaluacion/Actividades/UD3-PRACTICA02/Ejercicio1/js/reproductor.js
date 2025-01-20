adbc3b84d6d2d00376a87884fadf71ddd6a59510
Tony Abril <antabrrue@alu.edu.gva.es>
2025-01-21 00:06:07 +0100

    todo comentado y terminado

--- 2Evaluacion/Actividades/UD3-PRACTICA02/Ejercicio1/js/reproductor.js
+++ 2Evaluacion/Actividades/UD3-PRACTICA02/Ejercicio1/js/reproductor.js
@@ -42,20 +42,20 @@ function accionPlay()
 
 function saltarVideo(tiempo){
 
-	var nuevoTiempo=medio.currentTime+tiempo;
-	medio.currentTime=nuevoTiempo;
-	redimensionaBarra();
+	var nuevoTiempo=medio.currentTime+tiempo;	//se suma el tiempo saltado
+	medio.currentTime=nuevoTiempo;	//se aplica el cambio
+	redimensionaBarra();	//se aplican los cambios en la barra
 }
 
 function silencia(){
-	medio.muted = !medio.muted; // esto es super simple si es true lo pone a false, y viceversa
-	medio.muted ? silenciar.value = 'escuchar' : silenciar.value = 'silenciar';
+	medio.muted = !medio.muted; // alterna el estado del booleano
+	medio.muted ? silenciar.value = 'escuchar' : silenciar.value = 'silenciar';	//le da el valor al boton dependiendo del booleano
 }
 
 function variarVolumen(variacion){
-	let nuevoVolumen = medio.volume + variacion;
+	let nuevoVolumen = medio.volume + variacion;	//se le suma la variacion al volumen actual
 
-	medio.volume = Math.min(Math.max(nuevoVolumen,0),1);
+	medio.volume = Math.min(Math.max(nuevoVolumen,0),1);	//se le aplica la suma limitada entre 0 y 1
 }
 
 function iniciar() 
@@ -79,14 +79,17 @@ function iniciar()
 
 	barra.addEventListener('click', desplazarMedio, false);
 
-	
+	//funcion de botones para adelantar y atrasar el video
 	retrasar.addEventListener('click', () => saltarVideo(-5), false);
 	adelantar.addEventListener('click', () => saltarVideo(5), false);
 	
+	//funcion de boton para reiniciar el video
 	reiniciar.addEventListener('click', () => saltarVideo(-medio.currentTime), false);
 
+	//funcion de boton para silenciar el video
 	silenciar.addEventListener('click', silencia, false);
 
+	//funcion de botones para subir y bajar el volumen
 	menosVolumen.addEventListener('click', () => variarVolumen(-0.1), false);
 	masVolumen.addEventListener('click', () => variarVolumen(0.1), false);
 	
--- 2Evaluacion/Actividades/UD3-PRACTICA02/Ejercicio2/js/mi_js.js
+++ 2Evaluacion/Actividades/UD3-PRACTICA02/Ejercicio2/js/mi_js.js
@@ -1,22 +1,24 @@
-function accionVolver() 
-{
-	var b = document.getElementById("ligthbox");
-	b.classList.add('lb-oculto');
+// Función para ocultar el lightbox
+function accionVolver() {
+    var b = document.getElementById("ligthbox"); // Obtiene el elemento ligthbox
+    b.classList.add('lb-oculto'); // Agrega la clase 'lb-oculto' para ocultar el lightbox
 }
-function mostrarLB(){
-	var b = document.getElementById("ligthbox");
-	b.classList.remove('lb-oculto');
+
+// Función para mostrar el lightbox
+function mostrarLB() {
+    var b = document.getElementById("ligthbox");  // Obtiene el elemento ligthbox
+    b.classList.remove('lb-oculto'); // Remueve la clase 'lb-oculto' para mostrar el lightbox
 }
 
 function iniciar() 
 {
 
-	volver = document.getElementById('volver');
-	lb_opcion = document.getElementById('opc_lb');
+	volver = document.getElementById('volver');	//guarda el boton volver
+	lb_opcion = document.getElementById('opc_lb');	//guarda el boton para abrir el lightbox
 	
-	volver.addEventListener('click', accionVolver, false);
-	lb_opcion.addEventListener('click', mostrarLB, false);
+	volver.addEventListener('click', accionVolver, false);	//añade funcion de volver
+	lb_opcion.addEventListener('click', mostrarLB, false);	//añade funcion de mostrar el contenido
 
 }
 
-window.addEventListener('load', iniciar, false);
+window.addEventListener('load', iniciar, false);	//carga el codigo de iniciar al cargarse la pagina
